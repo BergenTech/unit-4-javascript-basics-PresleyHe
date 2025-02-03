15accf4b798126fe9facb081b0cb092a2e176aec
Presley <prehe28@bergen.org>
2025-02-02 18:34:18 -0800

    Secret Number 1-10

---
+++ Lesson08-Conditionals/6-guess_the_secret_number_1-10/script.js
@@ -0,0 +1,44 @@
+// GENERATES RANDOM NUMBER
+let secretNumber = Math.floor(Math.random()*10) +1;
+
+// DETERMINES WHETHER IT IS EVEN OR ODD
+let OddAndEven = (secretNumber % 2)
+
+
+let guess = prompt("Guess the secret number between 1 and 10:")
+
+// CLARIFICATION OF NUMBER IF YOU ARE TESTING IT : DO NOT OPEN CONSOLE IF YOU ARE PLAYING
+console.log(secretNumber)
+
+// CORRECT GUESS MESSAGE
+if (parseInt(guess) === secretNumber) {
+    alert("Congratulations! You've got it!");
+} else if (parseInt(guess) > secretNumber) {
+    // TOO HIGH GUESS MESSAGE
+    alert("Sorry, the number is lower. Try again!");
+} else {
+    // TOO LOW GUESS MESSAGE
+    alert("Sorry, the number is higher. Try again!");
+    guess = prompt("Guess Again")
+    
+    
+    //DETERMINES WHETHER EVEN OR ODD
+    
+    
+    if (OddAndEven % 2 === 0) {
+        alert("Hint the Number is Even")
+    } else {
+        alert("Hint the Number is Odd")
+
+    }
+    // LAST TRY MESSAGE
+    guess = prompt("LAST TRY!")
+
+        
+        if (parseInt(guess) === secretNumber) {
+            alert("Congradulations you finally got it!")
+        } else {
+            // LOSS MESSAGE
+            alert(`YOU LOST! The number was ${secretNumber}`)
+        }
+}
